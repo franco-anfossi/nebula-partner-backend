a02e8a39f055c29a6f4f0459aa5703dbfd24bff2
Franco Anfossi <franco.anfossi@uc.cl>
2024-04-05 22:47:24 -0300

    add: basic users settings

--- backend/partner_backend/urls.py
+++ backend/partner_backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users.urls import urlpatterns as users_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users/', include(users_urls)),
 ]
