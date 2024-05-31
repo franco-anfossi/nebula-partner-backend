6c7b67a3582bc7a69db2bd60bb10e247a35d6f03
Franco Anfossi <franco.anfossi@uc.cl>
2024-05-31 01:11:46 -0400

    style(users): flake8, isort & black perfect

--- users/permissions.py
+++ users/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
-from .models import Company
+
 
 class IsCompanyUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return hasattr(request.user, 'company')
+        return hasattr(request.user, "company")
--- users/serializers.py
+++ users/serializers.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+
 from .models import Company, Employee
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ["username", "password"]
         extra_kwargs = {
-            'password': {'write_only': True},
+            "password": {"write_only": True},
         }
+
 
 class CompanySerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -16,32 +19,49 @@ class CompanySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Company
-        fields = ['id', 'rut', 'name', 'phone_number', 'email', 'password']
+        fields = ["id", "rut", "name", "phone_number", "email", "password"]
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User.objects.create_user(username=validated_data['rut'],
-                                        email=validated_data['email'],
-                                        password=password)
+        password = validated_data.pop("password")
+        user = User.objects.create_user(
+            username=validated_data["rut"],
+            email=validated_data["email"],
+            password=password,
+        )
 
         company = Company.objects.create(user=user, **validated_data)
         return company
 
+
 class EmployeeSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     email = serializers.EmailField(required=True)
-    employee_type = serializers.ChoiceField(choices=(('buyer', 'Buyer'), ('seller', 'Seller')), required=False)
+    employee_type = serializers.ChoiceField(
+        choices=(("buyer", "Buyer"), ("seller", "Seller")), required=False
+    )
 
     class Meta:
         model = Employee
-        fields = ['id', 'rut', 'name', 'phone_number', 'email', 'password', 'employee_type']
+        fields = [
+            "id",
+            "rut",
+            "name",
+            "phone_number",
+            "email",
+            "password",
+            "employee_type",
+        ]
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User.objects.create_user(username=validated_data['rut'],
-                                        email=validated_data['email'],
-                                        password=password)
-        company_user = self.context['request'].user.company
-        validated_data.pop('company', None)
-        employee = Employee.objects.create(user=user, company=company_user, **validated_data)
+        password = validated_data.pop("password")
+        user = User.objects.create_user(
+            username=validated_data["rut"],
+            email=validated_data["email"],
+            password=password,
+        )
+        company_user = self.context["request"].user.company
+        validated_data.pop("company", None)
+        employee = Employee.objects.create(
+            user=user, company=company_user, **validated_data
+        )
         return employee
