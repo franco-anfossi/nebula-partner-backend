e62c66cc5a9f8fc780839aaa060e30f990c485e3
Franco Anfossi <franco.anfossi@uc.cl>
2024-11-09 00:13:25 -0300

    refactor(models): repr of the models

--- src/company/models.py
+++ src/company/models.py
@@ -12,13 +12,17 @@ class Company(Base):
     id = Column(Integer, primary_key=True, index=True)
     legal_name = Column(String(255), nullable=False)
     tax_id = Column(String(12), unique=True, nullable=False)
-    is_active = Column(Boolean, default=True)  # Activo/Inactivo
-    created_at = Column(DateTime, default=datetime.now(datetime.timezone.utc))  # Fecha de creación
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=lambda: datetime.now().astimezone())
 
     # Relaciones
     accounts = relationship("Account", back_populates="company")
     supplier = relationship("Supplier", uselist=False, back_populates="company")
-    buyer = relationship("Buyer", uselist=False, back_populates="company")
+    # buyer = relationship("Buyer", uselist=False, back_populates="company")
 
     def __repr__(self):
-        return f"<Company(legal_name={self.legal_name}, tax_id={self.tax_id}, is_active={self.is_active})>"
+        return (
+            f"<Company(name={self.legal_name}, "
+            f"tax_id={self.tax_id}, "
+            f"active={self.is_active})>"
+        )
